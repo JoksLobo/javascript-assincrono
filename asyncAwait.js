a06a66ed9a74d8183cde369967eb68cbbdf41a16
JoksLobo <jocson_herry@hotmail.com>
2026-03-17 00:37:10 -0300

    async e await

---
+++ asyncAwait.js
@@ -0,0 +1,31 @@
+// //sintaxe async function nomeDaFunca(){... await(esperar uma promessa ser resolvida)}
+// async function exemplo() {
+//   console.log("Iniciamos um programa.");
+
+//   await esperar(2000);
+//   console.log("Dado 1 recebido pode continuar o programa."); //login
+
+//   await esperar(1000);
+//   console.log("Dado 1 recebido - agora recebi o dado 2"); //login - ver uma foto
+
+//   console.log("Fim do programa.");
+// }
+
+// function esperar(ms) {
+//   return new Promise((resolve) => setTimeout(resolve, ms));
+// }
+
+// exemplo();
+
+//busca dados de servidor - https://jsonplaceholder.typicode.com/
+async function buscarDados() {
+  try {
+    await fetch("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  } catch (error) {
+    console.error("Erro:", error);
+  }
+}
+
+buscarDados();
